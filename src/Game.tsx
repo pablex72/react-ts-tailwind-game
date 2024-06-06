fd22cbf4da6c1c1a56055054ececdafab37b4695
pablo <tonyp2hoy@gmail.com>
2024-06-06 12:00:47 -0400

    feat:adding text/click functionality

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-// import './App.css'
-
+import Square from "./Square";
+const INITIAL_GAME_STATE = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
 function Game() {
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
+  const handleCellClick = (event:any) => {
+    console.log("cell clicked!!!!!", event.target.getAttribute("data-cell-index"));
+  };
+
   return (
     <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-center text-5xl mb-4 font-display text-white">
         Tic Tac Toe Game
       </h1>
       <div>
-        <div>Board Goes Here</div>
+        <div className="grid grid-cols-3 gap-3 mx-auto w-96">
+          {gameState.map((player, index) => (
+            <Square key={index} onClick={handleCellClick} {...{ index, player }} />
+          ))}
+        </div>
         <div>Scores go here</div>
       </div>
     </div>
