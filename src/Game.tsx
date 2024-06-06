e15e14eb2190dec65a7da917f2f5912a163f49e4
pablo <tonyp2hoy@gmail.com>
2024-06-06 12:55:33 -0400

    feat:adding change player function

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,28 +1,81 @@
 import { useEffect, useState } from "react";
 import Square from "./Square";
 const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""];
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 function Game() {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
-  const [currentPlayer, setCurrentPlayer] = useState("X")
+  const [currentPlayer, setCurrentPlayer] = useState("X");
 
-  useEffect(()=>{
-    changePlayer();
-  }, [gameState])
 
-  const changePlayer = () =>{
-    setCurrentPlayer(currentPlayer === "X"?"0":"X")
+  useEffect(() => {
+    checkForWinner();
+  }, [gameState]);
+
+  const resetBoard = () => setGameState(INITIAL_GAME_STATE)
+
+  const handleWin = () => {
+    window.alert(`Congrats player ${currentPlayer}! You are the winner`);
+    resetBoard()
+  };
+
+  const handleDraw = () =>{
+    window.alert("The game ended");
+    resetBoard()
   }
-  const handleCellClick = (event:any) => {
+
+  const checkForWinner = () => {
+    let roundWon = false;
+
+    for (let i = 0; i < WINNING_COMBOS.length; i++) {
+      const winCombo = WINNING_COMBOS[i];
+
+      let a = gameState[winCombo[0]];
+      let b = gameState[winCombo[1]];
+      let c = gameState[winCombo[2]];
+
+      if ([a, b, c].includes("")) {
+        continue;
+      }
+
+      if (a === b && b === c) {
+        roundWon = true;
+        break;
+      }
+    }
+    if (roundWon) {
+      setTimeout(() => handleWin(), 500);
+      window.alert(`Congrats player ${currentPlayer}! You are the winner!!!`);
+      return;
+    }
+    if (!gameState.includes("")) {
+      setTimeout(() => handleDraw(), 500);
+      return;
+    }
+    changePlayer();
+  };
+
+  const changePlayer = () => {
+    setCurrentPlayer(currentPlayer === "X" ? "0" : "X");
+  };
+  const handleCellClick = (event: any) => {
     // console.log("cell clicked!!!!!", event.target.getAttribute("data-cell-index"));
-    const cellIndex = Number(event.target.getAttribute("data-cell-index"))
+    const cellIndex = Number(event.target.getAttribute("data-cell-index"));
     const currentValue = gameState[cellIndex];
-    if(currentValue){
-      return
+    if (currentValue) {
+      return;
     }
     const NewValues = [...gameState];
     NewValues[cellIndex] = currentPlayer;
     setGameState(NewValues);
-    
   };
 
   return (
@@ -33,7 +86,11 @@ function Game() {
       <div>
         <div className="grid grid-cols-3 gap-3 mx-auto w-96">
           {gameState.map((player, index) => (
-            <Square key={index} onClick={handleCellClick} {...{ index, player }} />
+            <Square
+              key={index}
+              onClick={handleCellClick}
+              {...{ index, player }}
+            />
           ))}
         </div>
         <div>Scores go here</div>
